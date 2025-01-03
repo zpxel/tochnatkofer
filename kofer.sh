7393189e1e3d4ae2aa005ffe476bee705a5597c1
zpxel <zpixeljy@gmail.com>
2025-01-03 21:06:40 +0800

    Add files via upload

---
+++ kofer.sh
@@ -0,0 +1,17 @@
+pkg update
+pkg upgrade
+pkg install python
+pkg install python-pip
+pip install pycryptodome
+pkg install git
+pkg install zip
+termux-setup-storage
+pkg install p7zip
+cd
+cd tochnatkofer
+ls
+mv funny.mp4 /data/data/com.termux/files/home/storage/downloads
+cd
+cd tochnatkofer
+chmod +x tochnatkofer.py
+python tochnatkofer.py
